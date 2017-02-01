4290f43e2861c76c11c37233764d63c7ca8ecfd6
Distra Vantari <distravantari@gmail.com>
2017-02-01 11:08:34 +0700

    add admin react app

---
+++ client/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import store from './store'
+import routes from './config/routes'
+
+import 'styles_path/style.sass'
+import 'themes_root/js/jquery.bxslider.js'
+import 'themes_root/js/retina-1.1.0.min.js'
+// import 'themes_root/js/main.js'
+
+render(
+  <Provider store={store}>{routes}</Provider>,
+  document.getElementById('app')
+)
